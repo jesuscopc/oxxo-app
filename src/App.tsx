6c5a1ffefa08e19920f5aa7c1c1ed04221f107bb
Jesus <jesusmr@vivaldi.net>
2021-07-29 23:46:32 -0500

    chore: First config repo from scratch

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import OxxoApp from './root/OxxoApp';
+import './index.scss';
+
+const App = (): React.ReactElement => {
+  return <OxxoApp />;
+};
+
+export default App;
---
+++ src/root/OxxoApp.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OxxoApp = (): React.ReactElement => {
+  return <h1>Oxxo app working</h1>;
+};
+
+export default OxxoApp;
